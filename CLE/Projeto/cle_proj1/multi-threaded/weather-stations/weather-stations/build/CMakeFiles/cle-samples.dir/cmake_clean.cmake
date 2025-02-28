861cde492d88e8815f88f202b74d22a2ffbe06ae
João Gaspar <j.gaspar@ua.pt>
2025-02-28 19:41:07 +0000

    cle

---
+++ CLE/Projeto/cle_proj1/multi-threaded/weather-stations/weather-stations/build/CMakeFiles/cle-samples.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cle-samples.dir/src/create-samples.cpp.o"
+  "CMakeFiles/cle-samples.dir/src/create-samples.cpp.o.d"
+  "cle-samples"
+  "cle-samples.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cle-samples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CLE/Projeto/cle_proj1/single-threaded/weather-stations/weather-stations/build/CMakeFiles/cle-ws.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cle-ws.dir/src/main.cpp.o"
+  "CMakeFiles/cle-ws.dir/src/main.cpp.o.d"
+  "cle-ws"
+  "cle-ws.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cle-ws.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
